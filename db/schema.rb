faafd9c8bfb213ecff8514b56fd2a0bdd322b9de
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-03-29 17:10:10 +0100

    added email column to business card

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170329150630) do
+ActiveRecord::Schema.define(version: 20170329160841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170329150630) do
     t.datetime "updated_at",           null: false
     t.integer  "user_id"
     t.string   "company_name"
+    t.string   "email"
     t.index ["user_id"], name: "index_business_cards_on_user_id", using: :btree
   end
 
