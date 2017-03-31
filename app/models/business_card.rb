7c28d8f15ae1ff48f5846014583babea43e09bab
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-03-31 22:56:29 +0100

    now displaying correct tags for all situations

--- app/models/business_card.rb
+++ app/models/business_card.rb
@@ -6,4 +6,15 @@ class BusinessCard < ApplicationRecord
   def full_name
     "#{self.first_name} #{self.last_name}"
   end
+
+  def tags_to_display(business_card, owner_id, current_user_id)
+    tags = []
+    business_card.tag_relations.where(creator_id: owner_id).each do |relation|
+      tags << Tag.find(relation.tag_id)
+    end
+    business_card.tag_relations.where(creator_id: current_user_id).each do |relation|
+      tags << Tag.find(relation.tag_id)
+    end
+    return tags.uniq
+  end
 end
