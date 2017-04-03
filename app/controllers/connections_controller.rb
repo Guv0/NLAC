f6676d68ab4307ffe2d51be0e394a6dfe03f1120
Romain Campech <romain.campech@city.ac.uk>
2017-04-03 11:59:31 +0100

    Added contacts list (sortable)

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -1,6 +1,12 @@
 class ConnectionsController < ApplicationController
 
+helper_method :sort_column, :sort_direction
+
   def index
+    contacts_ids =  current_user.contacts do |contact|
+                      contact.id
+                    end
+    @contacts = BusinessCard.where(user_id: contacts_ids).order(sort_column + ' ' + sort_direction)
   end
 
   def create
@@ -12,4 +18,15 @@ class ConnectionsController < ApplicationController
     redirect_to business_card_connections_path(current_user)
   end
 
+private
+
+  def sort_column
+    BusinessCard.column_names.include?(params[:sort]) ? params[:sort] : "last_name"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"
+  end
+
 end
+
