cb98e62fe5048b93b4d49ed5f42c93b5fb796610
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-04-03 16:43:11 +0100

    edit form includes photo upload, navbar avatar updated

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,4 +1,17 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
+  process eager: true  # Force version generation at upload time.
+
+  process convert: 'jpg'
+
+  version :standard do
+    cloudinary_transformation effect: "brightness:30",
+      width: 200, height: 200, crop: :thumb, gravity: :face
+  end
+
+  version :navbar do
+    cloudinary_transformation effect: "brightness:30",
+      width: 40, height: 40, crop: :thumb, gravity: :face
+  end
 end
