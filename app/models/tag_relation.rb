c5ebd49ec2a176833d75debb1adcee0515c9cdc8
Romain Campech <romain.campech@city.ac.uk>
2017-04-04 14:13:42 +0100

    Added scopes to tag_relations -> search working sexy fine

--- app/models/tag_relation.rb
+++ app/models/tag_relation.rb
@@ -1,6 +1,9 @@
 class TagRelation < ApplicationRecord
-  belongs_to :business_card
-  belongs_to :tag
+
+belongs_to :business_card
+belongs_to :tag
+
+default_scope { where('creator_id = business_card_id OR creator_id = ?', "#{User.current.id}") }
 
   def add_tag(normalized_tag, business_card_id, current_user_id)
     if Tag.where(label: normalized_tag) == []
@@ -12,4 +15,5 @@ class TagRelation < ApplicationRecord
       TagRelation.create(tag_id: Tag.where(label: normalized_tag).first.id, business_card_id: business_card_id, creator_id: current_user_id)
     end
   end
+
 end
