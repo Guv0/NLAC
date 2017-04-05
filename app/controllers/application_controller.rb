e28f613d6f0edcb917599325fef1d0728677cc26
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-04-05 19:54:35 +0100

    redirects to business card after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,30 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_filter :set_current_user
+  after_filter :store_location
 
-def after_sign_in_path_for(resource)
-  request.env['omniauth.origin'] || stored_location_for(resource) || root_url
-end
+  def store_location
+    return unless request.get?
+    if (request.path != "/users/sign_in" &&
+        request.path != "/users/sign_up" &&
+        request.path != "/users/password/new" &&
+        request.path != "/login" &&
+        request.path != "/signup" &&
+        request.path != "/users/password/edit" &&
+        request.path != "/users/confirmation" &&
+        request.path != "/users/sign_out" &&
+        !request.xhr?) # don't store ajax calls
+      session[:previous_url] = request.fullpath
+    end
+  end
+
+
+
+  def after_sign_in_path_for(resource)
+
+      session[:previous_url] || root_path
+  end
+
 
 private
 
