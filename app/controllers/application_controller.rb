7cbcd74bc054d9239d9bb48f1895cc81baa4ed78
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-04-05 18:33:30 +0100

    adding contact from email logic done, only redirect back after sign in to complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,13 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_filter :set_current_user
 
+def after_sign_in_path_for(resource)
+  request.env['omniauth.origin'] || stored_location_for(resource) || root_url
+end
+
 private
 
   def set_current_user
     User.current = current_user
-  end
-
-  def after_sign_in_path_for(resource)
-    session["user_return_to"] || root_path
   end
 end
