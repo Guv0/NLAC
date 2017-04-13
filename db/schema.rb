7111e3ad6923f5649a617ce2849baf025b879f0a
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-04-13 09:41:47 +0100

    admin column added to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170403151914) do
+ActiveRecord::Schema.define(version: 20170413084010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 20170403151914) do
     t.string   "token"
     t.datetime "token_expiry"
     t.boolean  "company",                default: false
+    t.boolean  "admin",                  default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
