e92052963716ced8c785bcfb2bc88b85bfbc317b
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-04-26 17:41:38 +0100

    connection request model changed

--- app/models/connection_request.rb
+++ app/models/connection_request.rb
@@ -1,4 +1,26 @@
 class ConnectionRequest < ApplicationRecord
   belongs_to :user
-  has_one :contact, :foreign_key => :contact_id, class_name: 'User'
+  belongs_to :contact, class_name: 'User'
+
+  validates :user, presence: true
+  validates :contact, presence: true, uniqueness: { scope: :user }
+
+  validate :not_self
+  validate :not_pending
+  # validate :not_contacts
+
+  def accept
+    contactship.create(user: self.user, contact: self.contact)
+    destroy
+  end
+
+  private
+
+  def not_self
+    errors.add(:contact, "can't be equal to user") if user == contact
+  end
+
+  def not_pending
+    errors.add(:contact, "already requested connection") if contact.pending_friends.include?(user)
+  end
 end
