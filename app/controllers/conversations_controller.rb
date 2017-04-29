7d177a98a1cf29ce48971887d7164200ae216497
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-04-29 09:49:39 +0100

    messages controller and views

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,7 +2,7 @@ class ConversationsController < ApplicationController
   before_action :authenticate_user
 
   def index
-    @users = User.all
+    # @users = User.all
     @conversations = Conversation.all
   end
 
