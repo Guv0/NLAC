ba2c4deb4633dacb80b4ba29901918e0e67a6f27
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-04-29 16:50:06 +0100

    communities controller

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,8 +1,12 @@
 class CommunitiesController < ApplicationController
+  before_action :set_community, only: [:show, :update, :destroy]
+
   def index
+    @communities = Communities.all
   end
 
   def show
+    @members = User.find(CommunityMembership.where(community_id: @community.id).user_ids)
   end
 
   def new
@@ -19,4 +23,11 @@ class CommunitiesController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def set_community
+    @community = Community.find(params[:id])
+  end
 end
+
