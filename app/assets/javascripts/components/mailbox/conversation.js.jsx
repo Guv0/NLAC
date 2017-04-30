e4ce2d0f27aeb84d8e50e58e54ee1c3c872a912b
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-04-30 13:17:51 +0100

    react mailbox displaying everything except reply

--- app/assets/javascripts/components/mailbox/conversation.js.jsx
+++ app/assets/javascripts/components/mailbox/conversation.js.jsx
@@ -2,9 +2,15 @@ var Conversation = React.createClass({
   render: function() {
     console.log(this.props.conversation[3]);
     console.log(this.props.conversation[3][0]);
+    var messages = [];
+
+    this.props.conversation[3].map(function(message, i) {
+      messages.push(<Message message={message} key={i} />);
+    })
+
     return (
       <div>
-        {this.props.conversation[3][0].body}
+        {messages}
       </div>
     )
   }
