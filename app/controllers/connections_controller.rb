0c2b3a03075930743c16c5a8084d1ab4963b3fb4
Romain Campech <romain.campech@city.ac.uk>
2017-05-02 14:37:52 +0100

    Recent updates working all good + designed

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -79,7 +79,7 @@ skip_after_action :verify_authorized
       contact.business_card.updates.each do |update|
         update.each do | key, value |
           if key != "updated_at"
-            @updates << [contact.business_card.full_name, key, update[key][1], update["updated_at"][1]]
+            @updates << [contact.business_card, key, update[key][1], update["updated_at"][1]]
           end
         end
       end
