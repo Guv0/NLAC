f2a2a538761ca296d96def2223e46678a7970222
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-05-13 13:39:02 +0100

    all controllers updated

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,7 +1,5 @@
 class ConversationsController < ApplicationController
   before_action :authenticate_user!
-  skip_after_action :verify_policy_scoped
-  skip_after_action :verify_authorized
 
   def index
     conversations = Conversation.where(sender_id: current_user.id).or(Conversation.where(recipient_id: current_user.id))
