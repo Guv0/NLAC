9aacd3b540ef93a8f8a39d081572ed38c0c9f61a
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-05-28 17:34:45 +0100

    model associations

--- db/migrate/20170528160448_create_community_messages.rb
+++ db/migrate/20170528160448_create_community_messages.rb
@@ -3,8 +3,8 @@ class CreateCommunityMessages < ActiveRecord::Migration[5.0]
     create_table :community_messages do |t|
       t.string :title
       t.text :body
-      t.references :user, foreign_key: true
-      t.references :community, foreign_key: true
+      t.references :author, foreign_key: {to_table: :users}, index: true
+      t.references :community, foreign_key: true, index: true
 
       t.timestamps
     end
