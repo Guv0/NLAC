b104baeb574dc6993ead0ba8f2af8c9bf60ddb4c
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-06-01 18:00:51 +0100

    react business card set up ready to go, but with out AJAX

---
+++ app/assets/javascripts/components/business_card/business_card.js.jsx
@@ -0,0 +1,58 @@
+var BusinessCard = React.createClass({
+  getInitialState: function() {
+    return {
+      editable: false,
+      business_card: this.props.business_card
+    }
+  },
+
+  handleEdit: function(e) {
+    e.preventDefault();
+    this.setState({editable: true});
+  },
+
+  handleChange: function(e) {
+    e.preventDefault();
+    var business_card = this.state.business_card;
+    business_card[e.target.name] = e.target.value;
+    this.setState({business_card: business_card});
+  },
+
+  handleSave: function(e) {
+    console.log(this.state.business_card);
+    this.setState({editable: false});
+  },
+
+  renderNormal: function() {
+    return (
+      <div>
+        <div>First Name: {this.state.business_card.first_name}</div>
+        <div>Last Name: {this.state.business_card.last_name}</div>
+        <div>Email: {this.state.business_card.email}</div>
+        <div>City: {this.state.business_card.location}</div>
+        <button onClick={this.handleEdit}>Edit</button>
+      </div>
+    )
+  },
+
+  renderForm: function() {
+    return (
+      <div>
+        <div>First Name: <input name="first_name" placeholder={this.state.business_card.first_name} onChange={this.handleChange} ></input></div>
+        <input name="last_name" placeholder={this.state.business_card.last_name} onChange={this.handleChange} ></input>
+        <input name="location" placeholder={this.state.business_card.location} onChange={this.handleChange} ></input>
+        <input name="email" placeholder={this.state.business_card.email} onChange={this.handleChange} ></input>
+        <button onClick={this.handleSave}>Save</button>
+      </div>
+
+    )
+  },
+
+  render: function() {
+    if (this.state.editable) {
+      return this.renderForm()
+    } else {
+      return this.renderNormal()
+    }
+  }
+})
