7104c9cd902304d52eba93cbb116cc4018777b9d
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-06-05 13:44:54 +0100

    message post mailer method

--- app/mailers/community_mailer.rb
+++ app/mailers/community_mailer.rb
@@ -8,4 +8,15 @@ class CommunityMailer < ApplicationMailer
       subject:  "#{@community.name} created!"
     )
   end
+
+  def message_post(community)
+    @community = community
+
+    @community.members.each do |member|
+      mail(
+        to: member.email,
+        subject: "#{@community.name}'s manager posted a message"
+      )
+    end
+  end
 end
