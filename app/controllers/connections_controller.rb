51a9f02287794c2ac758cb5ff49339f89215cdfd
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-06-11 11:03:08 +0100

    pundit fix

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -2,7 +2,7 @@ class ConnectionsController < ApplicationController
 skip_before_action :authenticate_user!, only: [ :create, :guest_connection ]
 before_action :set_business_card, only: [ :index, :create, :destroy ]
 helper_method :sort_column, :sort_direction
-skip_after_action :verify_policy_scoped, only: [:create]
+skip_after_action :verify_policy_scoped, only: [:create, :index]
 skip_after_action :verify_authorized, only: [:create]
 
   def index
