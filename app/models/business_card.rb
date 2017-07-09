6ac0d4946d9145dd46b0ce0c73f4fb92879d7df3
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-07-09 14:38:38 +0100

    lat long done

--- app/models/business_card.rb
+++ app/models/business_card.rb
@@ -5,7 +5,7 @@ has_many :tag_relations, dependent: :destroy
 has_many :tags, through: :tag_relations
 
 geocoded_by :location
-after_validation :geocode, if: ->(self){ self.location.present? and self.location_changed? }
+after_validation :geocode, if: ->(business_card){ business_card.location.present? and business_card.location_changed? }
 
 include PgSearch
 pg_search_scope :search_for, against: [:first_name, :last_name, :location, :industry, :position, :company_name]
