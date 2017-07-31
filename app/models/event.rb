6aa32ea068adbbff0d0f3be6d6ba87e90d95d148
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-07-31 13:42:00 +0100

    new db sketch

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  belongs_to :community, :class_name => 'Community'
+  belongs_to :community
   has_one :organiser, :class_name => 'User'
-  has_many :attendees, :class_name => 'User'
+  has_many :attendees, :class_name => 'User', through: :communities
 end
