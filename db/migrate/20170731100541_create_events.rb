d4e85aaf240f7797f82c38c88450df4c60e0c2ab
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-07-31 11:25:55 +0100

    attendees_id column changed to attendee_ids

--- db/migrate/20170731100541_create_events.rb
+++ db/migrate/20170731100541_create_events.rb
@@ -4,8 +4,8 @@ class CreateEvents < ActiveRecord::Migration[5.0]
       t.string :title
       t.text :description
       t.references :community, index: true, foreign_key: true
-      t.references :organiser, index: true, foreign_key: true
-      t.references :attendees, index: true, foreign_key: true
+      t.references :organiser, index: true, foreign_key: {to_table: :users}
+      t.references :attendees, index: true, foreign_key: {to_table: :users}
       t.datetime :start_time
       t.datetime :end_time
 
