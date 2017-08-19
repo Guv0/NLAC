2d73a8d253df7fb3477c11c9b2b1b1122e7963d4
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-08-19 15:28:18 +0100

    event routes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,27 @@
 class EventsController < ApplicationController
+  before_action :set_event, except: [:new, :create]
 
   def show
   end
 
+  def new
+  end
 
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
 end
